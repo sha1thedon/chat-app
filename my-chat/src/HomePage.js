43e668142c6d5aa7b56464cf64e6887594392b84
Shawon Sheikh <shawon.sheikh@infinityworks.com>
2022-12-01 15:09:09 +0000

    shows up after submitting

--- my-chat/src/HomePage.js
+++ my-chat/src/HomePage.js
@@ -10,13 +10,15 @@ const socket = new WebSocket ('ws://localhost:8082')
 
 
 const HomePage = () => {
-  const [ message, setMessage] = useState('')
-  const messInput = useRef(null)
+  const [ message, setMessage] = useState([])
+  
 
-    const sendToServer = (event) => {
-      socket.send('hello')
-      event.preventDefault()
-      setMessage(event.target.value)
+    const sendToServer = () => {
+      // setMessage()
+      //onSubmit()
+      socket.send(message[0])
+      // event.preventDefault()
+      console.log('sendToServer: ', message[0])
     }
 
     //  const buttonClicked = () => {
@@ -33,32 +35,37 @@ const HomePage = () => {
 
     const onInputHandler = (event) => {
       if(validMessage(event.target.value)){
-          setMessage(event.target.value)
+          setMessage([event.target.value])
+          console.log('onInputHandler if: ', event.target.value)
       } else {
-          setMessage('')
+          setMessage([])
+          console.log('onInputHandler else: ', event.target.value)
       }
-  }
+    }
 
     const messageInput = () => {
       return (
         <div>
-          <input ref = {messInput} onChange={onInputHandler} placeholder='Send a message'/>
+          <input onChange={onInputHandler} placeholder='Send a message'/>
           <button onClick={sendToServer} className='send-message'>Send Message</button>
         </div>
       )
     }
 
+    const onSubmit = () => {
+      return <div>{message[0]}</div>
+    }
    
       
     socket.onmessage = ({ data }) => {
       console.log('message from server', data)
       console.log(data)
     }
-     
+     console.log(message)
       return (
         <div className="App">
-          <h1>Message: {message}</h1>
           {messageInput()}
+          {message[0]}
           
         </div>
       );
